d3c3db5f8fdb459e0a0121c4a7a0bff983fc5271
LightWarrior <andrey159852@gmail.com>
2022-01-21 18:15:51 +0200

    replaced the comma with a dot

--- js-twitter-card-ui/script.js
+++ js-twitter-card-ui/script.js
@@ -1,20 +1,29 @@
 let followed = false;
 
-const dataFollowersNode = document.querySelector('[data-followers]').lastElementChild;
+const dataFollowersNode = document.querySelector('[data-followers]');
 const dataButtonNode = document.querySelector('[data-button]');
 
 document.querySelector('[data-button]').addEventListener('click', () => {
-    let dataFollowersNumber = +dataFollowersNode.textContent;
+    let dataFollowersNumber = dataFollowersNode.textContent.replace(',', '');
 
     if (followed) {
-        dataFollowersNode.textContent = --dataFollowersNumber;
+        --dataFollowersNumber;
+        dataFollowersNode.textContent = getdataFollowersString(dataFollowersNumber);
         dataButtonNode.textContent = 'Follow';
         dataButtonNode.style.backgroundColor = '#2888cb';
         followed = false;
     } else {
-        dataFollowersNode.textContent = ++dataFollowersNumber;
+        ++dataFollowersNumber;
+        dataFollowersNode.textContent = getdataFollowersString(dataFollowersNumber);
         dataButtonNode.textContent = 'Following';
         dataButtonNode.style.backgroundColor = '#34cf7a';
         followed = true;
+    }
+
+    function getdataFollowersString(number) {
+        const dataFollowersArray = number.toString().split('');
+        dataFollowersArray.splice(3, 0, ',');
+        const dataFollowersString = dataFollowersArray.join('');
+        return dataFollowersString;
     }
 });
